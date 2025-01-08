93ed572657753e467064127a7b4498f25e8b0f7d
klipina53 <klipina53@gmail.com>
2025-01-09 02:30:40 +0500

    Реализованно подключение к базе данных.Липина

---
+++ 43/Context/TasksContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using _43.Classes.Database;
+using _43.Models;
+
+namespace _43.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+    }
+}
